80f30ecba749e32683a7a785669232cc9c6263d1
Pedrosa10 <56645360+Pedrosa10@users.noreply.github.com>
2026-09-20 09:53:58 -0300

    Update simples-lente-prioridade.user.js
    Teste atualização

--- simples-lente-prioridade.user.js
+++ simples-lente-prioridade.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Simples Lente - Prioridade de Pedidos
+// @name         Bling - Prioridade de Pedidos
 // @namespace    https://simpleslente.com.br/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Sistema de pedidos Urgentes e Importantes da Simples Lente no Bling
 // @match        https://www.bling.com.br/*
 // @updateURL    https://raw.githubusercontent.com/Pedrosa10/simples-lente-bling/refs/heads/main/simples-lente-prioridade.user.js
